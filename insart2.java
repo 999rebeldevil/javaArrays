e3e27bbac50d53c9cf1368c6f693ec0ce5482c45
999rebeldevil <159004537+999rebeldevil@users.noreply.github.com>
2024-08-09 02:18:43 +0530

    java day1

---
+++ insart2.java
@@ -0,0 +1,18 @@
+public class insart2 {
+    public static void main(String[] args){
+
+        int[] intArray = new int[6];
+        int length = 0;
+        for(int i = 0; i < 3; i++){
+            intArray[length] = i;
+            length++;
+        }
+        for(int i = 0; i < intArray.length; i++){
+            System.out.println("Index "+ i +" contains " + intArray[i]);
+        }
+
+
+
+
+    }
+}
---
+++ insret.java
@@ -0,0 +1,19 @@
+public class insret {
+
+    public static void main(String[] args){
+        int[] a = {10, 20, 30, 40, 50};
+        int pos = 3;
+        int eliment = 100;
+        for(int i=a.length-1; i>pos-1; i--){
+            a[i]=a[i-1];
+
+        }
+        a[pos-1]=eliment;
+
+        for(int i=0; i<a.length; i++){
+            System.out.println(a[i]+" ");
+        }
+
+
+    }
+}
